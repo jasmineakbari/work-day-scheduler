2dc4eae26fa3832caa273a36b119e57ce5a70a45
unknown <jasmineakbari@yahoo.com>
2020-11-11 22:07:20 -0500

    small cleanup of html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 const today = moment();
+const events = {};
 
 // display date function
 var displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = today.format('ll');
+
+// function to add events
+//var addEvent = function() {
+
+//}
+
+// function to load events
+//var loadEvents = function() {
+    //events = JSON.parse(localStorage.getItem("events"));
+//}
+
+// function to save events
+//var saveEvents = function() {
+    //localStorage.setItem("events", JSON.stringify(events));
+//};
