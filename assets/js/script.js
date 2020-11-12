c957c50fbbffa345e7daf2606e1a70f4946a8f16
unknown <jasmineakbari@yahoo.com>
2020-11-11 20:32:43 -0500

    added time rows and columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,3 @@
+// const today = moment();
 
-// Current Day Function and array
-var months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-], d, m, y, now, out_string;
-
-now = new Date();
-d = now.getDate();
-m = now.getMonth();
-y = now.getFullYear();
-
-out_string = months[m] + ' ' + d + ' , ' + y;
-
-document.getElementById('currentDay').innerHTML = out_string;
+// today.format('ll');
