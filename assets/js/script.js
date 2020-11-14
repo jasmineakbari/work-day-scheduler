65ad45a443307d650d659efa807c893c7f66840f
unknown <jasmineakbari@yahoo.com>
2020-11-14 15:05:06 -0500

    added check time function to add styling based on hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,48 @@
 const today = moment();
 // empty array to hold events
-var events = {};
+var events = [];
 
 // display date function
 var displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = today.format('ll');
 
+// get current time and format to hour
+var now = moment().format("H");
 
-$(".saveBtn").on("click", function() {
-    // get value of closest textarea and push to array
+// Check time function and apply corresponding class
+var checkTimes = function() {
+    // select all description block elements
+    var descriptionBlockElements = $(".description");
+
+    // loop through description classes
+    for (var i = 0; i < descriptionBlockElements.length; i++) {
+
+        // get description elements by ID
+        var manipID = document.getElementById(descriptionBlockElements[i].id)
+
+        // Get description arrays ID's as a string
+        var descriptionID = descriptionBlockElements[i].id;        
+
+        // remove any old classes from the element
+        $(descriptionBlockElements[i].id).removeClass(".present .past .future");
+
+        // apply new class if task is present/past/future
+        if (descriptionID < now) {
+            $(manipID).addClass("past");
+        } else if (descriptionID > now) {
+            $(manipID).addClass("future");
+        } else {
+            $(manipID).addClass("present");
+        }
+    }
+
+}
+
+// run checkTime every 5 minutes
+setInterval(checkTimes(), (1000 * 60) * 5);
+
+// Save Tasks function
+// $(".saveBtn").on("click", function() {
     
-    
-})
+
+// })
